47a90403828d640e7538ef6749e80197375df63d
Priyaanka-B <107568062+Priyaanka-B@users.noreply.github.com>
2022-06-16 18:44:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,10 @@ streamlit.header('break fast menu')
 streamlit.text('oats meal and juice')
 streamlit.text('Chicken Biriyani')
 streamlit.text('sweets and fruits')
+streamlit.text('sweets and fruits')
+
+
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
